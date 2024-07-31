45c4ace58a42bd71bdb9f62705cd2f79134acbf2
victorgilon <145818255+victorgilon@users.noreply.github.com>
2024-07-31 11:00:39 -0500

    ejercicios

--- js/27.js
+++ js/27.js
@@ -8,26 +8,35 @@ const producto = {
 
 // object constructor
 
+
+function Cliente(nombre, apellido){
+    this.nombre = nombre;
+    this.apellido = apellido;
+}
+
+Cliente.prototype.formatearCliente = function(){
+    return `el cliente ${this.nombre} ${this.apellido}`;
+}
+
 function Producto(nombre, precio){
     this.nombre = nombre;
-    this.precio = precio;
-
+    this.precio = precio;    
 }
+
 // Prototype crea funciones que solo se utilizan en un objeto en espesifico
 Producto.prototype.formatearProducto = function(){
-    return `el producto ${producto.nombre} tiene un precio de ${producto.precio}`;
+    return `el producto ${this.nombre} tiene un precio de ${this.precio}`;
 }
 
 
 const producto2 = new Producto('monito curvo de 49"', 800);
 const producto3 = new Producto('laptop', 300);
+const cliente = new Cliente('victor', 'gilon');
 
-function formatearProducto(producto){
-    return `el producto ${producto.nombre} tiene un precio de ${producto.precio}`;
-}
-
+console.log(cliente);
+console.log(producto2);
+console.log(cliente.formatearCliente());
 console.log(producto2.formatearProducto());
-console.log(producto3.formatearProducto());
 
 
 
---
+++ js/28.js
@@ -0,0 +1,11 @@
+// Clases
+
+class Producto {
+    constructor(nombre, precio){
+        this.nombre = nombre;
+        this.precio = precio;                
+    }
+}
+
+const producto2 = new Producto('monito curvo de 49"', 800);
+const producto3 = new Producto('laptop', 300);
